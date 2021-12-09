b454fcf6b7932ccfcdc42562ae298aa9009dd00a
Daniel Randall <digital-tectonivsinfo@gmail.com>
2021-12-08 22:42:52 -0500

    RawCameraSupport_CleanUp * Raw Camera Support Check only creates a single dialog with camera data

--- CameraXExtreme/app/src/main/java/com/digital_tectonics/cameraxextreme/constant/ExposureLevel.kt
+++ CameraXExtreme/app/src/main/java/com/digital_tectonics/cameraxextreme/constant/ExposureLevel.kt
@@ -8,13 +8,18 @@ package com.digital_tectonics.cameraxextreme.constant
 
 /**
  * ExposureLevel
+ * Used to adjust the Exposure level based on the actual exposure value (EV) range
+ *  supported by the camera.
+ *
  *
  * @author Daniel Randall on 2021-12-02.
  */
 enum class ExposureLevel(exposureValue: Int) {
-    DARK_MAX(-5),
-    DARK_STEP(-1),
-    NETURAL(0),
-    LIGHT_STEP(1),
-    LIGHT_MAX(5)
+    DARK_EV_MAX(-5),
+    DARK_WHOLE_EV(-3),
+    DARK_EV_STEP(-1),
+    NETURAL_EV(0),
+    LIGHT_EV_STEP(1),
+    LIGHT_WHOLE_EV(3),
+    LIGHT_EV_MAX(5)
 }
